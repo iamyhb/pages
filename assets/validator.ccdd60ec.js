1032a5deb3cc7c428e05192cf2b40b182ff6615d
yuhongbo <YHB@DESKTOP-7986EO4>
2022-05-06 14:35:26 +0800

    feature(all): test2 base页面
    test2 base页面

---
+++ assets/validator.ccdd60ec.js
@@ -0,0 +1,1 @@
+import{e as t}from"./index.9713a9ce.js";const a=["year","month","date","dates","week","datetime","datetimerange","daterange","monthrange"],n=e=>["",...t].includes(e),s=e=>[...a].includes(e);export{s as a,n as i};
