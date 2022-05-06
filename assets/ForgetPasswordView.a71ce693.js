0494a795fad980361343bd3a6015c112b5d2dad2
yuhongbo <YHB@DESKTOP-7986EO4>
2022-05-06 14:10:58 +0800

    feature(all): 更新页面
    更新页面

---
+++ assets/ForgetPasswordView.a71ce693.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vue_export-helper.21dcd24c.js";import{o as r,c as o}from"./index.5ad0d671.js";const t={};function c(a,n){return r(),o("h1",null,"Forget")}var f=e(t,[["render",c]]);export{f as default};
